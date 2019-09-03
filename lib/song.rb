27eaa93f156d935c64ba2d8afdfae86f06a320c7
Sara McCombs <thesaramccombs@gmail.com>
2019-09-03 21:01:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,12 @@
-class Song 
+class Song
+  attr_accessor :song_name
   
+  def initialize(song_name)
+    @@song_count += 1 
+    @song_name = song_name
+  end
+  
+  def self.count
+    @@song_count
+  end
 end
