ee512fdbf591893728b3fec5aa04b225e5f788a0
Sara McCombs <thesaramccombs@gmail.com>
2019-09-03 21:11:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,12 @@ class Song
   attr_accessor :name, :artists, :genres
   @@count = 0 
   @@genres = []
+  @@artist = []
   
-  def initialize(name, genre)
+  def initialize(name, artist, genre)
     @@count += 1 
     @name = name
+    if @@artist.include?(artist) ? 
     if @@genres.include?(genre) ? @@genres[genre] += 1 : @@genres[genre] = 1 
   end
   
@@ -15,5 +17,9 @@ class Song
   
   def self.genres
     @@genres.keys
+  end
+  
+  def self.genre_count
+    @@genres
   end
 end
